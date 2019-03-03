6047b4b018e084393b67e11835b6dd73e6827313
eitanrich <eitan.richardson@gmail.com>
2019-03-03 21:35:01 +0200

    Creating a HTML with a list of clusters

--- create_clusters_html.py
+++ create_clusters_html.py
@@ -2,8 +2,10 @@ import os
 import numpy as np
 import pickle as pkl
 from matplotlib import pyplot as plt
+from collections import Counter
 
 clusters = pkl.load(open(r'./data/clusters_id_list.p', 'rb'))
+id_to_year = pkl.load(open(r'./data/all_ids_to_years.p', 'rb'))
 
 header = """
 <html>
@@ -44,7 +46,16 @@ footer = """
 with open('./output/list_clusters.html', 'w') as outf:
     print(header, file=outf)
     for i, c in enumerate(clusters):
-        print('<div class="gallery">', file=outf)
+        c_years = Counter([id_to_year[id] for id in c])
+        recent_part = (c_years.get(2018, 0) + c_years.get(2017, 0)) / float(len(c))
+        is_very_hot = len(c) > 100 and recent_part > 0.65
+        is_hot = len(c) > 100 and recent_part > 0.5
+        if is_very_hot:
+              print('<div style="background-color:red" class="gallery">', file=outf)
+        elif is_hot:
+              print('<div style="background-color:blue" class="gallery">', file=outf)
+        else:
+            print('<div class="gallery">', file=outf)
         print('  <a target="_blank" href="clusters/{}/papers_list.txt">'.format(i), file=outf)
         print('    <img src="clusters/{}/wordcloud.jpg" alt="Forest" width="600" height="400">'.format(i), file=outf)
         print('  </a>', file=outf)
